d35c1f193c63ae49e4ae6989473d8d9c99dcc5c3
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-13 00:57:07 +0700

    Phase 10A: add IDocQrView contract

---
+++ PCTP/Presentation/Views/IDocQrView.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Data;
+
+namespace PCTP.Presentation.Views
+{
+    /// <summary>
+    /// UI contract for the QR/document scanning workflow.
+    /// </summary>
+    public interface IDocQrView
+    {
+        void BindDocQRCode(DataTable dt);
+        void SwitchToDocQRView();
+        bool HoiXoaDocQR();
+        void ClearQRInput();
+
+        string QRCodeInput { get; }
+        int SttDangSuaSl { get; }
+        int GetFocusedDocQRStt();
+        (string LotFcc, int SlFcc, int SlHvn) GetFocusedDocQRTemInfo();
+        void DeleteFocusedDocQRRow();
+        void ClearDocQRRows();
+
+        int? ShowSuaSoLuongTem(int sttBan, string lotFcc, int slFcc, int slHvn);
+        int? GetSuaSoLuongResult();
+
+        event EventHandler DocQRCodeClicked;
+        event EventHandler<string> QRCodeSubmitted;
+        event EventHandler XoaDongQRClicked;
+        event EventHandler XoaToanBoQRClicked;
+        event EventHandler SuaSoLuongTemClicked;
+    }
+}
