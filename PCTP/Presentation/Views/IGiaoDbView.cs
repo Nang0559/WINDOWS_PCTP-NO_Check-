cf95d6c1c98b40a68a34a5089d2b70d3ca927260
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-13 01:01:09 +0700

    Phase 10B: inherit common UI feedback contract

--- PCTP/Presentation/Views/IGiaoDbView.cs
+++ PCTP/Presentation/Views/IGiaoDbView.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Data;
 
 namespace PCTP.Presentation.Views
@@ -7,15 +6,13 @@ namespace PCTP.Presentation.Views
     /// <summary>
     /// UI contract for the GiaoDB delivery scenario.
     /// </summary>
-    public interface IGiaoDbView
+    public interface IGiaoDbView : IViewFeedback
     {
         void SwitchToPhieuDBView();
         void XoaDongGiaoDB();
         void ThemDongGiaoDB(DataTable danhSachMaHang);
         void UpdateGioXuatFromDB(string gioFCC);
-
         bool IsLoaiSP { get; }
-
         event EventHandler UploadGiaoDBClicked;
         event EventHandler LuuGiaoDBClicked;
     }
