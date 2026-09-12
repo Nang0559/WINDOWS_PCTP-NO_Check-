dc252d0b6d53a7501355afabb70c3b30d85f5e13
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-13 01:01:04 +0700

    Phase 10B: inherit common UI feedback contract

--- PCTP/Presentation/Views/IDocQrView.cs
+++ PCTP/Presentation/Views/IDocQrView.cs
@@ -6,23 +6,20 @@ namespace PCTP.Presentation.Views
     /// <summary>
     /// UI contract for the QR/document scanning workflow.
     /// </summary>
-    public interface IDocQrView
+    public interface IDocQrView : IViewFeedback
     {
         void BindDocQRCode(DataTable dt);
         void SwitchToDocQRView();
         bool HoiXoaDocQR();
         void ClearQRInput();
-
         string QRCodeInput { get; }
         int SttDangSuaSl { get; }
         int GetFocusedDocQRStt();
         (string LotFcc, int SlFcc, int SlHvn) GetFocusedDocQRTemInfo();
         void DeleteFocusedDocQRRow();
         void ClearDocQRRows();
-
         int? ShowSuaSoLuongTem(int sttBan, string lotFcc, int slFcc, int slHvn);
         int? GetSuaSoLuongResult();
-
         event EventHandler DocQRCodeClicked;
         event EventHandler<string> QRCodeSubmitted;
         event EventHandler XoaDongQRClicked;
