07d9e36f993055f1144d4ec3aa95480f85ea1ecd
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-13 01:01:40 +0700

    Phase 10B: preserve GiaoDB behavior

--- PCTP/Presentation/Presenters/GiaoDbPresenter.cs
+++ PCTP/Presentation/Presenters/GiaoDbPresenter.cs
@@ -18,7 +18,7 @@ namespace PCTP.Presentation.Presenters
             _c.RunWithLoadingSync(() => _c.PhieuSvc.XuLySauUploadGiaoDB(), "Đang xử lý đơn hàng GIAO DB...");
             _c.UiContext.Post(_ => _c.LoadPhieuGiaoDB(), null);
         }
-        private void OnLuuGiaoDB(object sender, EventArgs e) => _c.RunWithLoadingSync(() => { _v.SwitchToPhieuDBView(); _c.PhieuSvc.LuuGiaoDB(_c.View.GetDonHangTable(), _c.GioXuatHienTai, _c.AddNM); _v.ShowInfo("Xong !!!"); }, "Đang lưu dữ liệu giao DB...");
+        private void OnLuuGiaoDB(object sender, EventArgs e) => _c.RunWithLoadingSync(() => { _c.PhieuSvc.LuuGiaoDB(_c.PhieuView.GetDonHangTable(), _c.GioXuatHienTai, _c.AddNM); _v.ShowInfo("Xong !!!"); }, "Đang lưu dữ liệu giao DB...");
         public void Dispose() { _v.UploadGiaoDBClicked -= OnUploadGiaoDB; _v.LuuGiaoDBClicked -= OnLuuGiaoDB; }
     }
 }
