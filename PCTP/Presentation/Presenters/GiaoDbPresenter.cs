f99b67f25ca5f8e2feb21b931d5716d8b5ee454b
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-13 01:01:32 +0700

    Phase 10B: narrow GiaoDbPresenter view dependency

--- PCTP/Presentation/Presenters/GiaoDbPresenter.cs
+++ PCTP/Presentation/Presenters/GiaoDbPresenter.cs
@@ -1,5 +1,6 @@
 using PCTP.Applications.Services;
 using PCTP.Modules.GiaoHangKhach.SubForm;
+using PCTP.Presentation.Views;
 using System;
 using System.Windows.Forms;
 
@@ -8,7 +9,8 @@ namespace PCTP.Presentation.Presenters
     internal sealed class GiaoDbPresenter : IDisposable
     {
         private readonly HVNPresenterContext _c;
-        internal GiaoDbPresenter(HVNPresenterContext context) { _c = context; _c.View.UploadGiaoDBClicked += OnUploadGiaoDB; _c.View.LuuGiaoDBClicked += OnLuuGiaoDB; }
+        private readonly IGiaoDbView _v;
+        internal GiaoDbPresenter(HVNPresenterContext context) { _c = context; _v = _c.GiaoDbView; _v.UploadGiaoDBClicked += OnUploadGiaoDB; _v.LuuGiaoDBClicked += OnLuuGiaoDB; }
         internal bool OnGiaoDBChanging(int addNm) { return true; }
         private void OnUploadGiaoDB(object sender, EventArgs e)
         {
@@ -16,7 +18,7 @@ namespace PCTP.Presentation.Presenters
             _c.RunWithLoadingSync(() => _c.PhieuSvc.XuLySauUploadGiaoDB(), "Đang xử lý đơn hàng GIAO DB...");
             _c.UiContext.Post(_ => _c.LoadPhieuGiaoDB(), null);
         }
-        private void OnLuuGiaoDB(object sender, EventArgs e) => _c.RunWithLoadingSync(() => { _c.PhieuSvc.LuuGiaoDB(_c.View.GetDonHangTable(), _c.GioXuatHienTai, _c.AddNM); _c.View.ShowInfo("Xong !!!"); }, "Đang lưu dữ liệu giao DB...");
-        public void Dispose() { _c.View.UploadGiaoDBClicked -= OnUploadGiaoDB; _c.View.LuuGiaoDBClicked -= OnLuuGiaoDB; }
+        private void OnLuuGiaoDB(object sender, EventArgs e) => _c.RunWithLoadingSync(() => { _v.SwitchToPhieuDBView(); _c.PhieuSvc.LuuGiaoDB(_c.View.GetDonHangTable(), _c.GioXuatHienTai, _c.AddNM); _v.ShowInfo("Xong !!!"); }, "Đang lưu dữ liệu giao DB...");
+        public void Dispose() { _v.UploadGiaoDBClicked -= OnUploadGiaoDB; _v.LuuGiaoDBClicked -= OnLuuGiaoDB; }
     }
 }
