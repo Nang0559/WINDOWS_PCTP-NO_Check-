78fef1c2cf4cb437a68734043700551e4a3b3b82
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-13 23:32:19 +0700

    refactor(giaohangkhach): route GiaoDB dialog through UI boundary

--- PCTP/Presentation/Presenters/GiaoDbPresenter.cs
+++ PCTP/Presentation/Presenters/GiaoDbPresenter.cs
@@ -1,8 +1,7 @@
 using PCTP.Applications.Services;
-using PCTP.Modules.GiaoHangKhach.SubForm;
+using PCTP.Modules.GiaoHangKhach.HVN.Controls;
 using PCTP.Presentation.Views;
 using System;
-using System.Windows.Forms;
 
 namespace PCTP.Presentation.Presenters
 {
@@ -10,15 +9,53 @@ namespace PCTP.Presentation.Presenters
     {
         private readonly HVNPresenterContext _c;
         private readonly IGiaoDbView _v;
-        internal GiaoDbPresenter(HVNPresenterContext context) { _c = context; _v = _c.GiaoDbView; _v.UploadGiaoDBClicked += OnUploadGiaoDB; _v.LuuGiaoDBClicked += OnLuuGiaoDB; }
+        private readonly GiaoDbControl _ui;
+
+        internal GiaoDbPresenter(HVNPresenterContext context)
+        {
+            _c = context ?? throw new ArgumentNullException(nameof(context));
+            _v = _c.GiaoDbView;
+            _ui = new GiaoDbControl();
+            _ui.Configure(_c.PhieuSvc);
+
+            _v.UploadGiaoDBClicked += OnUploadGiaoDB;
+            _v.LuuGiaoDBClicked += OnLuuGiaoDB;
+        }
+
         internal bool OnGiaoDBChanging(int addNm) { return true; }
+
         private void OnUploadGiaoDB(object sender, EventArgs e)
         {
-            using (var frm = new FRM_UploadGiaoDB(_c.PhieuSvc)) { if (frm.ShowDialog() != DialogResult.OK) return; }
-            _c.RunWithLoadingSync(() => _c.PhieuSvc.XuLySauUploadGiaoDB(), "Đang xử lý đơn hàng GIAO DB...");
+            if (_ui.ShowUploadDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            _c.RunWithLoadingSync(
+                () => _c.PhieuSvc.XuLySauUploadGiaoDB(),
+                "Đang xử lý đơn hàng GIAO DB...");
+
             _c.UiContext.Post(_ => _c.LoadPhieuGiaoDB(), null);
         }
-        private void OnLuuGiaoDB(object sender, EventArgs e) => _c.RunWithLoadingSync(() => { _c.PhieuSvc.LuuGiaoDB(_c.PhieuView.GetDonHangTable(), _c.GioXuatHienTai, _c.AddNM); _v.ShowInfo("Xong !!!"); }, "Đang lưu dữ liệu giao DB...");
-        public void Dispose() { _v.UploadGiaoDBClicked -= OnUploadGiaoDB; _v.LuuGiaoDBClicked -= OnLuuGiaoDB; }
+
+        private void OnLuuGiaoDB(object sender, EventArgs e)
+        {
+            _c.RunWithLoadingSync(
+                () =>
+                {
+                    _c.PhieuSvc.LuuGiaoDB(
+                        _c.PhieuView.GetDonHangTable(),
+                        _c.GioXuatHienTai,
+                        _c.AddNM);
+
+                    _v.ShowInfo("Xong !!!");
+                },
+                "Đang lưu dữ liệu giao DB...");
+        }
+
+        public void Dispose()
+        {
+            _v.UploadGiaoDBClicked -= OnUploadGiaoDB;
+            _v.LuuGiaoDBClicked -= OnLuuGiaoDB;
+            _ui.Dispose();
+        }
     }
 }
