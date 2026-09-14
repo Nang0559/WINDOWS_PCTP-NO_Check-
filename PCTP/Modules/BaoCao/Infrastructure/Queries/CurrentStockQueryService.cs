5daa16154c861ef448269db9d41ead2b33e136fa
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-14 19:01:42 +0700

    fix(baocao): add missing SqlParameter namespace

--- PCTP/Modules/BaoCao/Infrastructure/Queries/CurrentStockQueryService.cs
+++ PCTP/Modules/BaoCao/Infrastructure/Queries/CurrentStockQueryService.cs
@@ -3,6 +3,7 @@ using PCTP.Modules.BaoCao.Application.Contracts.Queries;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 
