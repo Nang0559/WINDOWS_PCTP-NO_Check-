91e3fad0fce0bd62cfe1beb2818cb4def6e50017
Nang0559 <140684756+Nang0559@users.noreply.github.com>
2026-09-16 20:59:50 +0700

    feat(giaohang): expose FIFO session lifecycle

--- PCTP/Modules/GiaoHangKhach/Services/IDocQRService.cs
+++ PCTP/Modules/GiaoHangKhach/Services/IDocQRService.cs
@@ -1,34 +1,27 @@
-﻿using PCTP.Domain.Entities;
+using PCTP.Domain.Entities;
 using PCTP.Shared.Helpers;
 using System;
 using System.Data;
 
 namespace PCTP.Modules.GiaoHangKhach.Services
 {
-    /// <summary>
-    /// Thin facade cho các thao tác QR delivery (bắn QR, đối chiếu, xoá dòng...).
-    /// Implementation: <see cref="DocQRService"/>.
-    /// </summary>
     public interface IDocQRService
     {
         bool IsBanSP { get; }
         bool IsBanOType { get; }
 
         void SetCheDoBanSP(bool isSP);
-
         void SetCheDoBan(string gioMoTa);
 
         int CountChuaDG();
-
         bool CoDocQRNao();
-
         DataTable LoadAll();
-
         void XoaDong(int stt);
-
         void XoaToanBo();
-
         void CapNhapSlHvn(int stt, int slMoi);
+
+        void InitializeFifo(DataTable orderRows);
+        void ResetFifo();
 
         ScanResult ProcessScan(
             string rawQr,
